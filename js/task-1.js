60901af536499f0285677afc8a0af9cfda7d2606
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2021-02-21 23:43:07 +0300

    Add files via upload

--- js/task-1.js
+++ js/task-1.js
@@ -5,10 +5,11 @@ console.log(`в списке ${categoriesNumberRef} категории`);
 const itemRef = document.querySelectorAll(".item");
 console.log(itemRef);
 
-itemRef.map((item) => {
+itemRef.map((...itemRef) => {
   const itemHeadRef = document.querySelectorAll("h2");
   const itemNameRef = itemHeadRef.textContent;
+  const lastChildItems = itemRef.lastChild.length;
   console.log(
-    `категория: ${itemNameRef}, количество элементов: ${itemRef.lastChild.length}`
+    `категория: ${itemNameRef}, количество элементов: ${lastChildItems}`
   );
 });
