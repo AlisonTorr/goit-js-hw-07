7c524badfbf3a759cb7c37f953b7c2f11600af27
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2021-02-21 16:30:31 +0300

    Add files via upload

---
+++ js/task-1.js
@@ -0,0 +1,49 @@
+const catigories = [
+  {
+    name: "Животные",
+    list: ["Кот", "Хомяк", "Лошадь", "Попугай"],
+  },
+  {
+    name: "Продукты",
+    list: ["Хлеб", "Петрушка", "Творог"],
+  },
+  {
+    name: "Технологии",
+    list: ["HTML", "CSS", "JavaScript", "React", "Node"],
+  },
+];
+
+const createCatigoryCard = (catigory) => {
+  const catigoryCardRef = document.createElement("li");
+  catigoryCardRef.classList.add("item");
+
+  const titleRef = document.createElement("h2");
+  titleRef.textContent = catigory.name;
+
+  const catigoryListRef = document.createElement("ul");
+
+  const catigoryItemRef = document.createElement("li");
+  catigoryItemRef.textContent = catigory.list[i];
+
+  catigoryListRef.appendChild(catigoryItemRef);
+
+  catigoryCardRef.append(titleRef, catigoryListRef);
+
+  return catigoryCardRef;
+};
+
+const catigoryCards = catigories.map((catigory) =>
+  createCatigoryCard(catigory)
+);
+
+console.log(catigoryCards);
+
+const catigoriesRootRef = document.querySelector("#catigories");
+catigoriesRootRef.appendChild(catigoryCardRef);
+
+const catigoriesNumber = catigories.reduce((acc, catigory) => (acc += 1), 0);
+console.log(catigoriesNumber);
+
+const catigoryDesc = (catigories) =>
+  `Catigory name: ${catigory.name}, number of elements: ${catigory.list.length}`;
+console.log(catigories.catigoryDesc);
