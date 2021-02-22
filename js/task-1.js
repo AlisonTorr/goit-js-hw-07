8eb23e6844c9cebf42f534cc1cbca6ac7e9e4d94
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2021-02-22 17:39:39 +0300

    Add files via upload

--- js/task-1.js
+++ js/task-1.js
@@ -5,18 +5,7 @@ console.log(`в списке ${categoriesNumberRef} категории`);
 const itemRefs = document.querySelectorAll(".item");
 console.log(itemRefs);
 
-const itemHeadRef = document.querySelectorAll("h2");
-const itemHeadRefArr = Array.from(...itemHeadRef);
-const itemNameRef = itemHeadRefArr.forEach((head) => head.textContent);
-console.log(itemNameRef);
-
-const categoryDescRef = Array.from(...itemRefs);
-
-getCategoryDesc = categoryDescRef.forEach((ref) => {
-  const itemHeadRef = document.querySelector("h2");
-  const itemNameRef = itemHeadRef.textContent;
-  const lastChildItems = ref.lastChild.length;
-  return `категория: ${itemNameRef}, количество элементов: ${lastChildItems}`;
-});
-
-console.log(getCategoryDesc(itemRefs));
+for (const itemRef of itemRefs) {
+  return `категория: ${itemRef.firstElementChild.textContent} , 
+  количество элементов: ${itemRef.lastElementChild.children.length}`;
+}
