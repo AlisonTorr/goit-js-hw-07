c3f6980f4ab4a742884ef64cfc4ff224ee2ba7b0
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2021-02-22 13:09:00 +0300

    Add files via upload

--- js/task-5.js
+++ js/task-5.js
@@ -3,11 +3,9 @@ const inputRef = document.querySelector("#name-input");
 const nameOutputRef = document.querySelector("#name-output");
 const defaultOutPutRef = "незнакомец";
 
-const handleInputChange = (event) =>
-  (nameOutputRef.textContent = event.target.value);
+const handleInputChange = (event) => {
+  nameOutputRef.textContent =
+    inputRef === null ? defaultOutPutRef : event.target.value;
+};
 
-if (inputRef === null) {
-  nameOutputRef.textContent = defaultOutPutRef;
-} else {
-  inputRef.addEventListener("input", () => handleInputChange);
-}
+inputRef.addEventListener("input", () => handleInputChange);
