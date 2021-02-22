0f11711c2159c86b6ce7abe572b67054ad0cedc3
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2021-02-22 19:14:07 +0300

    Add files via upload

--- js/task-6.js
+++ js/task-6.js
@@ -16,10 +16,14 @@ inputRef.addEventListener("input", handleInputChange);
 
 console.log(inputLengthRef);
 
-const isValid = () => {
-  inputLengthRef === validInputLengthRef
+const validate = (str) => {
+  return str.length === validInputLengthRef;
+};
+
+const isValid = (event) => {
+  validate(event.target.value)
     ? inputRef.classList.add(".valid")
     : inputRef.classList.add(".invalid");
 };
 
-inputRef.addEventListener("blur", () => isValid(inputTextRef));
+inputRef.addEventListener("blur", isValid);
