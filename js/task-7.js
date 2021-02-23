c87adc593a451237dc1893b71b7f5424af1db7c6
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2021-02-23 15:00:40 +0300

    Add files via upload

--- js/task-7.js
+++ js/task-7.js
@@ -3,13 +3,12 @@ const currentFontSizeRef = controllerRef.value;
 console.log(currentFontSizeRef);
 
 const textRef = document.querySelector("#text");
-// const textValueRef = textRef.textContent;
+const textValueRef = textRef.textContent;
 
 const handleTextChange = (event) => {
   const value = event.target.value;
   console.log(value);
-  let textFontSizeRef = textRef.style.fontSize;
-  textFontSizeRef += value;
+  textValueRef.style.fontSize += value;
 };
 
 controllerRef.addEventListener("input", handleTextChange);
