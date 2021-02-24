c9b25e59a272ec592ca3f5b4a763a38299cfa74a
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2021-02-24 13:28:40 +0300

    Add files via upload

--- js/task-6.js
+++ js/task-6.js
@@ -1,14 +1,14 @@
 const inputRef = document.querySelector("#validation-input");
-inputRef.classList.add(".primary");
-const inputTextRef = inputRef.textContent;
-const validInputLengthRef = inputRef.getAttribute("data-length");
+inputRef.classList.add("primary");
+
+const validInputLengthRef = 6;
 
 const validate = (str) => {
   return str.length === validInputLengthRef;
 };
 
-const isValid = () => {
-  if (validate(inputTextRef)) {
+const isValid = (event) => {
+  if (validate(event.target.value.length)) {
     inputRef.classList.remove("invalid");
     inputRef.classList.add("valid");
   } else {
